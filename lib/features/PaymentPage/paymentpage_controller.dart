43052847a44cb529430132d4d855d9939c89660d
pawanbaghel01 <baghelpawan638@gmail.com>
2024-09-16 22:34:03 +0530

    All done

--- lib/features/PaymentPage/paymentpage_controller.dart
+++ lib/features/PaymentPage/paymentpage_controller.dart
@@ -1,64 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-// class PaymentPageController extends GetxController {
-//   FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-//   var payments = <Map<String, dynamic>>[].obs; // Observable list of payments
-//   var isLoading = false.obs;
-
-//   // Fetch payment details for the current vendor
-//   Future<void> fetchVendorPayments(String vendorId) async {
-//     try {
-//       isLoading.value = true;
-
-//       // Query the payments collection for the current vendor's payments
-//       QuerySnapshot snapshot = await _firestore
-//           .collection('payments')
-//           .where('vendorId', isEqualTo: vendorId)
-//           .get();
-
-//       // Clear the list before adding new data
-//       payments.clear();
-
-//       // Map the documents to a list of payment details
-//       snapshot.docs.forEach((doc) {
-//         payments.add(doc.data() as Map<String, dynamic>);
-//       });
-
-//     } catch (e) {
-//       print('Error fetching vendor payments: $e');
-//     } finally {
-//       isLoading.value = false;
-//     }
-//   }
-// }
-
-
 class PaymentPageController extends GetxController {
-  var isLoading = true.obs;
+  var isLoading = false.obs;
   var payments = [].obs;
 
-  // Fetch all payments from Firestore
-  void fetchAllPayments() async {
-    try {
-      isLoading(true);
-      QuerySnapshot snapshot = await FirebaseFirestore.instance
-          .collection('payments')
-          .get(); // Assuming all payments are in the 'payments' collection
+    // Fetch all payments from Firestore
+    void fetchAllPayments() async {
+      try {
+        isLoading(true);
+        QuerySnapshot snapshot = await FirebaseFirestore.instance
+            .collection('payments')
+            .get(); // Assuming all payments are in the 'payments' collection
 
-      // Map data to local list
-      payments.value = snapshot.docs.map((doc) {
-        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-        data['id'] = doc.id; // Store the document ID to update later
-        return data;
-      }).toList();
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to fetch payments: $e');
-    } finally {
-      isLoading(false);
+        // Map data to local list
+        payments.value = snapshot.docs.map((doc) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          data['id'] = doc.id; // Store the document ID to update later
+          return data;
+        }).toList();
+      } catch (e) {
+        Get.snackbar('Error', 'Failed to fetch payments: $e');
+      } finally {
+        isLoading(false);
+      }
     }
-  }
 
   // Update the amountReceived field in Firestore
   Future<void> updateAmountReceived(String paymentId, double newAmountReceived) async {
@@ -80,7 +46,7 @@ class PaymentPageController extends GetxController {
         payments.refresh(); // Refresh the UI
       }
 
-      Get.snackbar('Success', 'Amount received has been updated successfully.');
+      Get.snackbar('Success', 'Amount has been updated successfully.');
     } catch (e) {
       Get.snackbar('Error', 'Failed to update amount received: $e');
     }
--- lib/features/PaymentPage/paymentpage_view.dart
+++ lib/features/PaymentPage/paymentpage_view.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'paymentpage_controller.dart';
 
 class PaymentPageView extends StatelessWidget {
+  const PaymentPageView({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Instantiate the PaymentController
@@ -16,183 +18,188 @@ class PaymentPageView extends StatelessWidget {
     Color primaryColor = Colors.white;
     Color secondaryColor = Colors.orange;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Admin Payment Details'),
-        backgroundColor: secondaryColor,
-      ),
-      body: Obx(() {
-        // Show a loading indicator while fetching data
-        if (paymentController.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        // Check if there are no payments
-        // if (paymentController.payments.isEmpty) {
-        //   return const Center(
-        //     child: Text('No payment details available.'),
-        //   );
-        // }
-
-        // ListView to display payment details
-        return ListView.builder(
-          itemCount: paymentController.payments.length,
-          itemBuilder: (context, index) {
-            // Get payment details for the current item
-            Map<String, dynamic> payment = paymentController.payments[index];
-
-            // Extract payment details
-            String bookingId = payment['bookingId'] ?? 'N/A';
-            double totalAmount =
-                (payment['totalAmount'] as num).toDouble(); // Ensure double
-            double amountReceived =(payment['amountReceived']??00 as num).toDouble(); // Ensure double
-            double remainingAmount = totalAmount-amountReceived;
-            // TextEditingController for updating the amount received by admin
-            TextEditingController amountReceivedController =
-                TextEditingController(
-              text: amountReceived.toStringAsFixed(
-                  2), // Set the current amountReceived as the initial value
+    return GestureDetector(
+       onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Admin Payment Details'),
+          backgroundColor: secondaryColor,
+        ),
+        body: Obx(() {
+          // Show a loading indicator while fetching data
+          // if (paymentController.isLoading.value) {
+          //   return const Center(child: CircularProgressIndicator());
+          // }
+      
+          // Check if there are no payments
+          if (paymentController.payments.isEmpty) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-
-            // Check if payment is complete
-            bool isPaymentCompleted = amountReceived >= totalAmount;
-
-            return Card(
-              margin: const EdgeInsets.all(16.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-                side: BorderSide(
-                    color: secondaryColor, width: 1.5), // Thin orange border
-              ),
-              elevation: 5,
-              color: primaryColor, // Set card color to white (primary)
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Booking ID: $bookingId',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                        color: secondaryColor, // Orange color for Booking ID
-                      ),
-                    ),
-                    const SizedBox(height: 12.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Amount Sent :',
-                          style: TextStyle(fontSize: 16.0, color: Colors.black),
-                        ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(
-                          child: TextField(
-                            controller: amountReceivedController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              hintText: 'Enter Amount Received',
-                              border: OutlineInputBorder(),
-                            ),
-                            enabled:
-                                !isPaymentCompleted, // Disable the field if payment is completed
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8.0),
-                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Remaining Amount :',
-                          style: TextStyle(fontSize: 16.0, color: Colors.black),
-                        ),
-                        Text(
-                          '₹${remainingAmount.toStringAsFixed(2)}', // Using rupee symbol
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 16.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Total Amount :',
-                          style: TextStyle(fontSize: 16.0, color: Colors.black),
-                        ),
-                        Text(
-                          '₹${totalAmount.toStringAsFixed(2)}', // Using rupee symbol
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 16.0),
-                    isPaymentCompleted
-                        ? const Text(
-                            'Payment Completed',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0,
-                              color: Colors.green,
-                            ),
-                          )
-                        : Center(
-                          child: ElevatedButton(
-                              onPressed: () {
-                                double newAmountReceived = double.tryParse(
-                                        amountReceivedController.text) ??
-                                    amountReceived;
-                          
-                                // Logic to check if amountReceived is greater than totalAmount
-                                if (newAmountReceived > totalAmount) {
-                                  Get.snackbar(
-                                    'Error',
-                                    'Amount sent cannot be greater than the total amount.',
-                                    snackPosition: SnackPosition.BOTTOM,
-                                    backgroundColor: Colors.red,
-                                    colorText: Colors.white,
-                                  );
-                                } else if (newAmountReceived < amountReceived) {
-                                  Get.snackbar(
-                                    'Error',
-                                    'Amount sent cannot be less than the already received amount.',
-                                    snackPosition: SnackPosition.BOTTOM,
-                                    backgroundColor: Colors.red,
-                                    colorText: Colors.white,
-                                  );
-                                } else {
-                                  paymentController.updateAmountReceived(
-                                      payment['id'], newAmountReceived);
-                                }
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    secondaryColor, // Orange button color
-                              ),
-                              child: const Text('Update Amount Sent'),
-                            ),
-                        ),
-                  ],
+          }
+      
+          // ListView to display payment details
+          return ListView.builder(
+            itemCount: paymentController.payments.length,
+            itemBuilder: (context, index) {
+              // Get payment details for the current item
+              var payment = paymentController.payments[index];
+      
+              // Extract payment details
+              String bookingId = payment['bookingId'] ?? 'N/A';
+              double totalAmount =
+                  (payment['totalAmount'] as num).toDouble(); // Ensure double
+              double amountReceived =(payment['amountReceived']??00 as num).toDouble(); // Ensure double
+              double remainingAmount = totalAmount-amountReceived;
+              // TextEditingController for updating the amount received by admin
+              TextEditingController amountReceivedController =
+                  TextEditingController(
+                text: amountReceived.toStringAsFixed(
+                    2), // Set the current amountReceived as the initial value
+              );
+      
+              // Check if payment is complete
+              bool isPaymentCompleted = amountReceived >= totalAmount;
+      
+              return Card(
+                margin: const EdgeInsets.all(16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  side: BorderSide(
+                      color: secondaryColor, width: 1.5), // Thin orange border
                 ),
-              ),
-            );
-          },
-        );
-      }),
+                elevation: 5,
+                color: primaryColor, // Set card color to white (primary)
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Booking ID: $bookingId',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: secondaryColor, // Orange color for Booking ID
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Amount Sent :',
+                            style: TextStyle(fontSize: 16.0, color: Colors.black),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Expanded(
+                            child: TextField(
+                              controller: amountReceivedController,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter Amount Received',
+                                border: OutlineInputBorder(),
+                              ),
+                              enabled:
+                                  !isPaymentCompleted, // Disable the field if payment is completed
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8.0),
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Remaining Amount :',
+                            style: TextStyle(fontSize: 16.0, color: Colors.black),
+                          ),
+                          Text(
+                            '₹${remainingAmount.toStringAsFixed(2)}', // Using rupee symbol
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Total Amount :',
+                            style: TextStyle(fontSize: 16.0, color: Colors.black),
+                          ),
+                          Text(
+                            '₹${totalAmount.toStringAsFixed(2)}', // Using rupee symbol
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+                      isPaymentCompleted
+                          ? const Text(
+                              'Payment Completed',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: Colors.green,
+                              ),
+                            )
+                          : Center(
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  FocusScope.of(context).unfocus();
+                                  double newAmountReceived = double.tryParse(
+                                          amountReceivedController.text) ??
+                                      amountReceived;
+                            
+                                  // Logic to check if amountReceived is greater than totalAmount
+                                  if (newAmountReceived > totalAmount) {
+                                    Get.snackbar(
+                                      'Error',
+                                      'Amount sent cannot be greater than the total amount.',
+                                      snackPosition: SnackPosition.BOTTOM,
+                                      backgroundColor: Colors.red,
+                                      colorText: Colors.white,
+                                    );
+                                  } else if (newAmountReceived < amountReceived) {
+                                    Get.snackbar(
+                                      'Error',
+                                      'Amount sent cannot be less than the already received amount.',
+                                      snackPosition: SnackPosition.BOTTOM,
+                                      backgroundColor: Colors.red,
+                                      colorText: Colors.white,
+                                    );
+                                  } else {
+                                    paymentController.updateAmountReceived(
+                                        payment['id'], newAmountReceived);
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      secondaryColor, // Orange button color
+                                ),
+                                child: const Text('Update Amount Sent'),
+                              ),
+                          ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }),
+         ),
     );
   }
 }
